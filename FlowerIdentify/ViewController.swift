130c540f55cc73619a0fce1c6aa3cc765a18385f
psx95 <pranav.ps95@hotmail.com>
2019-03-10 12:09:54 +0530

    feat(ViewController): Uses coreML Model to classify flowers and display results on the navigation ba

--- FlowerIdentify/ViewController.swift
+++ FlowerIdentify/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -25,11 +27,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UI Image to CIImage")
+            }
+            detect(ciImage: ciImage)
         } else {
             print("Some error occured")
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func detect (ciImage: CIImage) {
+        do {
+            let model = try VNCoreMLModel(for: FlowerClassifier().model)
+            
+            let request = VNCoreMLRequest(model: model) { (vnRequest, error) in
+                guard let results = vnRequest.results as? [VNClassificationObservation] else {
+                    fatalError("Could not convert Results to classification")
+                }
+                
+                if let firstResult = results.first {
+                    print(firstResult.identifier)
+                    self.navigationItem.title = firstResult.identifier
+                }
+            }
+            
+            let handler = VNImageRequestHandler(ciImage: ciImage)
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        } catch {
+            print("Some error occured")
+        }
     }
 
 
